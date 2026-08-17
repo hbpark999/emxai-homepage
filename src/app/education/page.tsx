c8cc8930414e8762a2777f39bbb4d9d4ad528b45
hbpark999 <hbpark999@gmail.com>
2026-08-17 20:28:33 +0900

    Add slide4 image for final presentation in the education module

--- src/app/education/page.tsx
+++ src/app/education/page.tsx
@@ -1,5 +1,7 @@
 import { EducationSamples } from "@/components/education/education-samples";
 
+export const dynamic = "force-dynamic";
+
 export default function EducationPage() {
   return (
     <main className="flex-1 bg-white">
